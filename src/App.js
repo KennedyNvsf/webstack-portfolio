c8c354bf8a13393c6f5f322bedd1424415c1ff07
Webstack-Corporation <webstackcorp@gmail.com>
2021-09-22 10:23:11 +0100

    Mijan COde integration -> styling needs fixing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./styles/app.scss";
 //COMPONENTS
 import NavBar from "./components/navbar/NavBar";
 import IntroSection from "./components/introsection/IntroSection";
+import TeamMembers from "./components/TeamMembers/TeamMembers";
+import Vision from "./components/Vision/Vision";
+import Common from "./components/Common/Common";
+import {Marginer} from "./components/Marginer";
+import Mission from "./components/Mission/Mission";
+
 import { AnimatePresence } from "framer-motion"; //DETECTS WHEN THE MOUNTED COMPONENT LEAVES THE SCREEN.
 
 function App() {
@@ -15,6 +21,11 @@ function App() {
 
       <AnimatePresence exitBeforeEnter>
            <IntroSection/>
+           <TeamMembers/>
+           <Marginer direction="vertical" margin="4em"/>
+              <Mission/>
+           <Marginer direction="vertical" margin="4em"/>
+            <Vision/>
       </AnimatePresence>
     </div>
 
