5cf73d1cd98f9eb583c54a6509f3cbb7d004dfa7
Webstack-Corporation <webstackcorp@gmail.com>
2021-09-29 12:23:48 +0100

    Website base Layout Complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import NavBar from "./components/navbar/NavBar";
 import IntroSection from "./components/introsection/IntroSection";
 import TeamMembers from "./components/TeamMembers/TeamMembers";
 import Vision from "./components/Vision/Vision";
-import Common from "./components/Common/Common";
+// import Common from "./components/Common/Common";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
+
 import {Marginer} from "./components/Marginer";
 import Mission from "./components/Mission/Mission";
 import ServicesSection from "./components/servicesOffered/ServicesSection";
@@ -23,12 +26,22 @@ function App() {
       <AnimatePresence exitBeforeEnter>
            <IntroSection/>
            <TeamMembers/>
+
            <Marginer direction="vertical" margin="4em"/>
               <Mission/>
            <Marginer direction="vertical" margin="4em"/>
             <Vision/>
-            <ServicesSection/>
-      </AnimatePresence>
+            <Marginer direction="vertical" margin="3em" />
+              <ServicesSection/>
+            <Marginer direction="vertical" margin="3em" />
+             <Testimonials />
+
+       </AnimatePresence>
+       
+            <Marginer direction="vertical" margin="6em" />
+             <Footer />
+            <Marginer direction="vertical" margin="1em" />
+      
     </div>
 
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,134 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useMediaQuery } from "react-responsive";
+import { Marginer } from "../Marginer";
+import {
+  faDribbble,
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
+const Footer = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  return (
+    <>
+      {isMobile && (
+        <>
+          <div className="footer">
+            <div className="newsletter">
+              <h3>Subscribe to our Newsletter</h3>
+              <p>
+                Subscribe to our newsletter to regulerly get pro web development
+                tips and follow our progress.
+              </p>
+              <div className="input">
+                <input
+                  type="text"
+                  className="input"
+                  placeHolder="email@example.com"
+                />
+                <button>Subscribe now</button>
+              </div>
+            </div>
+            <div className="about-us">
+              <h3>About us</h3>
+              <ul>
+                <li>
+                  {" "}
+                  <p> Our Profile</p>
+                </li>
+                <li>
+                  {" "}
+                  <p>Our Mission </p>
+                </li>
+                <li>
+                  {" "}
+                  <p>Our Vision </p>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-left">
+              <h2>
+                <span className="logo-web">Web</span>
+                <span className="logo-stack">Stack</span>
+              </h2>
+              <p>Over 8,912,000 Satisfied with our appearance</p>
+              <div className="footer-icons">
+                <FontAwesomeIcon icon={faFacebook} />
+                <FontAwesomeIcon icon={faInstagram} />
+                <FontAwesomeIcon icon={faLinkedin} />
+                <FontAwesomeIcon icon={faDribbble} />
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+            </div>
+          </div>
+          <Marginer direction="vertical" margin="3em" />
+          <div className="copyright">
+            <p>
+              ©️ Copyright 2021 <span className="logo-web-footer">Web</span>
+              <span className="logo-stack-footer">Stack</span>
+            </p>
+          </div>
+        </>
+      )}
+      <div className="footer">
+        <div className="footer-left">
+          <h2>
+            <span className="logo-web">Web</span>
+            <span className="logo-stack">Stack</span>
+          </h2>
+          <p>Over 8,912,000 Satisfied with our appearance</p>
+          <div className="footer-icons">
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faDribbble} />
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+        </div>
+        <div className="about-us">
+          <h3>About us</h3>
+          <ul>
+            <li>
+              {" "}
+              <p> Our Profile</p>
+            </li>
+            <li>
+              {" "}
+              <p>Our Mission </p>
+            </li>
+            <li>
+              {" "}
+              <p>Our Vision </p>
+            </li>
+          </ul>
+        </div>
+        <div className="newsletter">
+          <h3>Subscribe to our Newsletter</h3>
+          <p>
+            Subscribe to our newsletter to regulerly get pro web development
+            tips and follow our progress.
+          </p>
+          <div className="input">
+            <input
+              type="text"
+              className="input"
+              placeHolder="email@example.com"
+            />
+            <button>Subscribe now</button>
+          </div>
+        </div>
+      </div>
+      <Marginer direction="vertical" margin="3em" />
+      <div className="copyright">
+        <p>
+          ©️ Copyright 2021 <span className="logo-web-footer">Web</span>
+          <span className="logo-stack-footer">Stack</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
